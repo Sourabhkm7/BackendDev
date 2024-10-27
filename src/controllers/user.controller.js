f3aac5e4bb5b9e18d9842b06c197c1eaa41925ee
Sourabhkm7 <sourabhhithaldia@gmail.com>
2024-10-27 23:32:01 +0530

    Add getWatchhistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -378,6 +378,54 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
      .json(new apiResponse(200, channel[0], "User Channel fetched successfully"))
 })
 
+ 
+const getWatchHistory = asyncHandler(async (req,res) =>{
+     const user = await User.aggregate([
+          {
+               $match:{
+                    _id: new mongoose.Types.ObjectId(req.user._id)
+               }
+          },
+          {
+               $lookup:{
+                   from: "videos",
+                   localField: 'watchHistory',
+                   foreignField: "_id",
+                   as: "watchHistory",
+                   pipeline:[ 
+                    {
+                         $lookup: {
+                              from: "user",
+                              localField: "owner",
+                              foreignField: "_id",
+                              as: "owner",
+                              pipeline:[{
+                                   $project: {
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                   }
+                         }]
+                         }
+                    },
+                    {
+                         $addFields:{
+                              owner:{
+                                   $first: "$owner"
+                              }
+                         }
+                    }
+                   ]
+               }
+          }
+     ])
+
+     return res
+          .status(200)
+          .json(new apiResponse(200, user[0].watchHistory, "Watch history fetched successfully"))
+})
+
+
 
 export  {
      registerUser, 
@@ -389,6 +437,7 @@ export  {
      updateAccountDetails,
      updateUserAvatar,
      updateUserCoverImage,
-     getUserChannelProfile
+     getUserChannelProfile,
+     getWatchHistory
 
 }
